567081af9ae5f44c22a4050a9bfab2d9c622f15f
Victor Eduardo Scherer <victorescherer@hotmail.com>
2021-06-28 19:23:52 -0300

    Fix tables.sql

--- back-end/sql/tables.sql
+++ back-end/sql/tables.sql
@@ -1,31 +1,6 @@
-CREATE TABLE IF NOT EXISTS restaurant (
-	Id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(300) NOT NULL
+CREATE TABLE Product (
+	Id BIGINT IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(200),
+	Value numeric (16,2),
+	ImageURL varchar(1000)
 );
-
-CREATE TABLE IF NOT EXISTS vote (
-	Id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    RestaurantId BIGINT NOT NULL,
-	ProfessionalId BIGINT NOT NULL,
-    Timestamp DATETIME NOT NULL,
-    CONSTRAINT FK_vote_Restaurant FOREIGN KEY (RestaurantId) REFERENCES restaurant(Id)
-);
-
-CREATE TABLE IF NOT EXISTS winner (
-	Id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    RestaurantId BIGINT NOT NULL,
-	Timestamp DATETIME NOT NULL,
-    CONSTRAINT FK_winner_Restaurant FOREIGN KEY (RestaurantId) REFERENCES restaurant(Id)
-);
-
-
-INSERT INTO restaurant (Nome) VALUES 
-("Madero"),
-("McDonalds"),
-("Sushito"),
-("BurgerKing"),
-("Vó Zilda"),
-("Don Peligroso"),
-("Churrascaria"),
-("Bobs"),
-("Pizza");
